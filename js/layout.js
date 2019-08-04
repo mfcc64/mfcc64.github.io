1733070bafca40a20febc3a9ef8d53ecb1c92ce3
Muhammad Faiz <mfcc64@gmail.com>
2019-08-04 08:31:16 +0700

    js/layout.js: add noopener on target _blank

--- js/layout.js
+++ js/layout.js
@@ -5,6 +5,8 @@
     footer.className = "align-center";
     footer.id = "footer-link";
     footer.innerHTML = "<a href='/'><img src='/img/icon/icon-32.png' alt='Home'/></a>" +
-                       "<a href='https://github.com/mfcc64' target='_blank'><img src='/img/icon/github-mark-light-32.png' alt='GitHub'/></a>";
+                       "<a href='https://github.com/mfcc64' rel='noopener' target='_blank'>" +
+                           "<img src='/img/icon/github-mark-light-32.png' alt='GitHub'/>" +
+                       "</a>";
     body.appendChild(footer);
 })();
