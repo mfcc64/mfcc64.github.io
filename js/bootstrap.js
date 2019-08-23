7360168e5928c3084fcf6ffe79f2ad0d82ef053a
Muhammad Faiz <mfcc64@gmail.com>
2019-08-23 11:38:04 +0700

    js/bootstrap.js: use local files on localhost

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -21,13 +21,17 @@ window.addEventListener("load", function(event){
     include_icon("/img/icon/icon-32.png", 32);
     include_icon("/img/icon/icon-48.png", 48);
     include_icon("/img/icon/icon-64.png", 64);
-    include_js("https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_HTML-full", true);
+    if (window.location && location.hostname == "localhost") {
+        include_js("/MathJax/MathJax.js?config=TeX-AMS_HTML-full", true);
+    } else {
+        include_js("https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS_HTML-full", true);
 
-    // analytics
-    var aid = "UA-146336275-1";
-    include_js("https://www.googletagmanager.com/gtag/js?id=" + aid, true);
-    gtag('js', new Date());
-    gtag('config', aid);
+        // analytics
+        var aid = "UA-146336275-1";
+        include_js("https://www.googletagmanager.com/gtag/js?id=" + aid, true);
+        gtag('js', new Date());
+        gtag('config', aid);
+    }
 
     include_js("/js/post.js", false);
     include_js("/js/layout.js", false);
