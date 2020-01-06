81b75b4966e841a4402f1da75cea2c8340d4079e
Muhammad Faiz <mfcc64@gmail.com>
2020-01-06 13:05:45 +0700

    js: improve layout

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,26 +1,27 @@
 
-window.addEventListener("load", function(event){
-    function include_icon(url, size) {
-        var icon = document.createElement("link");
-        icon.rel = "icon";
-        icon.type = "image/png";
-        icon.href = url;
-        icon.sizes = size + "x" + size;
-        document.head.appendChild(icon);
-    }
+function include_js(url, is_async) {
+    var js = document.createElement("script");
+    js.async = is_async;
+    js.src = url;
+    document.head.appendChild(js);
+}
 
-    function include_js(url, is_async) {
-        var js = document.createElement("script");
-        js.async = is_async;
-        js.src = url;
-        document.head.appendChild(js);
-    }
+function include_icon(url, size) {
+    var icon = document.createElement("link");
+    icon.rel = "icon";
+    icon.type = "image/png";
+    icon.href = url;
+    icon.sizes = size + "x" + size;
+    document.head.appendChild(icon);
+}
 
+(function(){
     include_icon("/img/icon/icon-16.png", 16);
     include_icon("/img/icon/icon-24.png", 24);
     include_icon("/img/icon/icon-32.png", 32);
     include_icon("/img/icon/icon-48.png", 48);
     include_icon("/img/icon/icon-64.png", 64);
+
     if (window.location && location.hostname == "localhost") {
         include_js("/MathJax/MathJax.js?config=TeX-AMS_HTML-full", true);
     } else {
@@ -34,10 +35,8 @@ window.addEventListener("load", function(event){
     }
 
     include_js("/js/post.js", false);
-    include_js("/js/layout.js", false);
-    if (document.getElementById("content-list"))
-        include_js("/js/content-list.js");
-});
+    include_js("/js/layout.js", true);
+})();
 
 // analytics
 window.dataLayer = window.dataLayer || [];
--- js/layout.js
+++ js/layout.js
@@ -1,12 +1,23 @@
 
-(function(){
+function default_layout() {
     var body = document.body;
-    var footer = document.createElement("div");
-    footer.className = "align-center";
-    footer.id = "footer-link";
-    footer.innerHTML = "<a href='/'><img src='/img/icon/icon-32.png' alt='Home'/></a>" +
-                       "<a href='https://github.com/mfcc64' rel='noopener' target='_blank'>" +
-                           "<img src='/img/icon/github-mark-light-32.png' alt='GitHub'/>" +
-                       "</a>";
-    body.appendChild(footer);
-})();
+    var content = document.getElementById("content");
+    var content_list =document.getElementById("content-list");
+
+    if (body && (content || content_list)) {
+        var footer = document.createElement("div");
+        footer.className = "align-center";
+        footer.id = "footer-link";
+        footer.innerHTML = "<a href='/'><img src='/img/icon/icon-32.png' alt='Home'/></a>" +
+                            "<a href='https://github.com/mfcc64' rel='noopener' target='_blank'>" +
+                                "<img src='/img/icon/github-mark-light-32.png' alt='GitHub'/>" +
+                            "</a>";
+        body.appendChild(footer);
+        if (content_list)
+            include_js("/js/content-list.js", true);
+    } else {
+        setTimeout(default_layout, 30);
+    }
+}
+
+default_layout();
