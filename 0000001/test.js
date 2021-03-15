27a2480f0fb6c56a43072027c4a0cd66598c5a57
Muhammad Faiz <mfcc64@gmail.com>
2021-03-16 06:12:04 +0700

    0000001/audio-worklet-test.html: add latency hint

--- 0000001/test.js
+++ 0000001/test.js
@@ -5,7 +5,16 @@ async function run_test() {
     if (global_audio_ctx)
         return;
 
-    var ctx = global_audio_ctx = new AudioContext();
+    var latency_hint = document.getElementById("latency-hint").value;
+    if (latency_hint == "")
+        latency_hint = "interactive";
+    if (latency_hint != "interactive" && latency_hint != "balanced" && latency_hint != "playback")
+        latency_hint = latency_hint * 1.0;
+
+    var ctx = global_audio_ctx = new AudioContext({latencyHint: latency_hint});
+    document.getElementById("base-latency").textContent =
+        "Latency hint = " + latency_hint + ", base latency = " + ctx.baseLatency +
+        " = " + Math.round(ctx.baseLatency * ctx.sampleRate) + "/" + ctx.sampleRate;
     await ctx.audioWorklet.addModule("gen-wave.js");
     var gen_wave = new AudioWorkletNode(ctx, "gen-wave",
                                         {numberOfInputs: 0, numberOfOutputs: 1, outputChannelCount: [2]});
